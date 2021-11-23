d82cbf4553d3dfb6e043e6b5e951cd059bdc1b15
syahrolus <msyahrul15aulia@gmail.com>
2021-11-23 08:35:15 +0800

    update

--- views/form.php
+++ views/form.php
@@ -44,12 +44,42 @@ include('../layouts/navbar.php');
             </form>
         </div>
     </div>
-    
+
+    <div class="row">
+        <div class="col-sm-4">
+            <div class="alert alert-success" role="alert">
+                <h4 class="alert-heading">Well done!</h4>
+                <p>Aww yeah, you successfully read this important alert message. This example text is going to run a bit
+                    longer so that you can see how spacing within an alert works with this kind of content.</p>
+                <hr>
+                <p class="mb-0">Whenever you need to, be sure to use margin utilities to keep things nice and tidy.</p>
+            </div>
+        </div>
+        <div class="col-sm-4">
+            <div class="alert alert-success" role="alert">
+                <h4 class="alert-heading">Well done!</h4>
+                <p>Aww yeah, you successfully read this important alert message. This example text is going to run a bit
+                    longer so that you can see how spacing within an alert works with this kind of content.</p>
+                <hr>
+                <p class="mb-0">Whenever you need to, be sure to use margin utilities to keep things nice and tidy.</p>
+            </div>
+        </div>
+        <div class="col-sm-4">
+            <div class="alert alert-success" role="alert">
+                <h4 class="alert-heading">Well done!</h4>
+                <p>Aww yeah, you successfully read this important alert message. This example text is going to run a bit
+                    longer so that you can see how spacing within an alert works with this kind of content.</p>
+                <hr>
+                <p class="mb-0">Whenever you need to, be sure to use margin utilities to keep things nice and tidy.</p>
+            </div>
+        </div>
+    </div>
+
 </div>
 
 
 
-    
+
 <?php 
 include('../layouts/foot.php');
 ?>
